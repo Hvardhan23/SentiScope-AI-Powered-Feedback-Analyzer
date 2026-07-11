9dd6fbf24cdf030010966120e31a385d394c24b0
Hvardhan23 <hareehvardhanchennoju@gmail.com>
2026-07-11 16:19:38 +0530

    Auto-sync: file changes

---
+++ hardware_perp.py
@@ -0,0 +1,8 @@
+import torch
+
+def get_device():
+    """
+    Returns the appropriate device for PyTorch operations.
+    If a GPU is available, it returns 'cuda', otherwise it returns 'cpu'.
+    """
+    return torch.device("cuda" if torch.cuda.is_available() else "cpu")
