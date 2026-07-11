85cd3dd12ed43729e7d0ea17950c88cf0b7b7d55
Hvardhan23 <hareehvardhanchennoju@gmail.com>
2026-07-11 16:01:59 +0530

    Auto-sync: file changes

---
+++ authentication.py
@@ -0,0 +1,13 @@
+import os
+from huggingface_hub import login
+
+def authenticate_huggingface():
+    """
+    Authenticate with Hugging Face using the API key from the .env file.
+    """
+    # Load the API key from the .env file
+    api_key = os.getenv("HFace_key")
+
+    print("Authenticating with Hugging Face...")
+    login(token=api_key)
+    print("Authentication successful!")
